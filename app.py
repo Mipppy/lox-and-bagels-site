2f271e062d33da2d300706aca02d615668d3bfe7
Mipppy <122882588+Mipppy@users.noreply.github.com>
2023-11-28 20:48:07 -0500

    Started project.
    Set up SQL database for users and have a half functioning user login/register/logout.  Also a nice little dropdown effect on load :)

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, flash, redirect, render_template, request, session, g
+from cs50 import SQL
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+# import os, stripe
+# stripe.api_key = os.environ['STRIPE_SECRET_KEY']
+
+DATABASE = 'sql.db'
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+db = SQL("sqlite:///sql.db")
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route('/')
+def index():
+   username = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+   return render_template("index.html", username=username[0]['username'])
+ 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        if not request.form.get("username"):
+            flash("You must provide a username!")
+            return render_template("register.html")
+        elif not request.form.get("password"):
+            flash("You must provide a password!")
+            return render_template("register.html")
+        elif not request.form.get("confirmation"):
+            flash("You must confirm your password!")
+            return render_template("register.html")
+        
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        if len(rows) == 1:
+            flash("Username taken!")
+            return render_template("register.html")
+        if request.form.get("password") != request.form.get("confirmation"):
+            flash("Passwords don't match!")
+            return render_template("register.html")
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",request.form.get("username"),generate_password_hash(request.form.get("password")),)
+        return redirect("/")
+    else:
+        return render_template("register.html")
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Enter a username!")
+            return render_template("login.html")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("Enter a password!")
+            return render_template("login.html")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            flash("Invalid username/password!")
+            return render_template("login.html")
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+if __name__ == '__main__':
+   app.run()
